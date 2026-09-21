3ac83750c1265a8d204bbb60b32aba22745adbcc
devwithbhatt <aman@bhattsquare.com>
2026-09-21 11:50:19 +0530

    add settings forms and UI templates for gym profile and payments

--- apps/settings/forms.py
+++ apps/settings/forms.py
@@ -5,8 +5,22 @@ class PaymentSettingForm(forms.ModelForm):
     class Meta:
         model = PaymentSetting
         fields = ['bank_name', 'account_holder_name', 'account_number', 'ifsc_code', 'qr_code']
+        widgets = {
+            'qr_code': forms.FileInput(attrs={'class': 'form-control', 'accept': 'image/*'}),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        for field in self.fields.values():
+        for field_name, field in self.fields.items():
             field.required = False
+            if field_name != 'qr_code':
+                field.widget.attrs.update({'class': 'form-control'})
+        
+        if 'bank_name' in self.fields:
+            self.fields['bank_name'].widget.attrs['placeholder'] = 'e.g. HDFC Bank, State Bank of India'
+        if 'account_holder_name' in self.fields:
+            self.fields['account_holder_name'].widget.attrs['placeholder'] = 'e.g. FitStack Fitness Hub / John Doe'
+        if 'account_number' in self.fields:
+            self.fields['account_number'].widget.attrs['placeholder'] = 'e.g. 50100234567890'
+        if 'ifsc_code' in self.fields:
+            self.fields['ifsc_code'].widget.attrs['placeholder'] = 'e.g. HDFC0001234'
